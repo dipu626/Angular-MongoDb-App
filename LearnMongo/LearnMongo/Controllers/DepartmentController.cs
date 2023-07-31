8a373542f4261c019751363c28a4a59f8af1e99e
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-31 20:19:59 +0600

    Backend implemented and Frontend created

---
+++ LearnMongo/LearnMongo/Controllers/DepartmentController.cs
@@ -0,0 +1,71 @@
+﻿using LearnMongo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace LearnMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient mongoClient = new(configuration.GetConnectionString("EmployeeAppCon"));
+
+            MongoDB.Driver.Linq.IMongoQueryable<Department> dbList = mongoClient.GetDatabase("testDB").GetCollection<Department>("Department").AsQueryable();
+
+            return new JsonResult(dbList);
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody] Department department)
+        {
+            MongoClient mongoClient = new(configuration.GetConnectionString("EmployeeAppCon"));
+            IMongoCollection<Department> dbContext = mongoClient.GetDatabase("testDB").GetCollection<Department>("Department");
+
+            int lastDepartmentId = dbContext.AsQueryable().Count();
+            department.DepartmentId = lastDepartmentId + 1;
+
+            dbContext.InsertOne(department);
+
+            return new JsonResult("Added successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put([FromBody] Department department)
+        {
+            MongoClient mongoClient = new(configuration.GetConnectionString("EmployeeAppCon"));
+            IMongoCollection<Department> dbContext = mongoClient.GetDatabase("testDB").GetCollection<Department>("Department");
+
+            FilterDefinition<Department> filter = Builders<Department>.Filter.Eq("DepartmentId", department.DepartmentId);
+            UpdateDefinition<Department> update = Builders<Department>.Update.Set("DepartmentName", department.DepartmentName);
+
+            dbContext.UpdateOne(filter, update);
+
+            return new JsonResult("Updated successfully");
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public JsonResult Delete(int id)
+        {
+            MongoClient mongoClient = new(configuration.GetConnectionString("EmployeeAppCon"));
+            IMongoCollection<Department> dbContext = mongoClient.GetDatabase("testDB").GetCollection<Department>("Department");
+
+            FilterDefinition<Department> filter = Builders<Department>.Filter.Eq("DepartmentId", id);
+
+            dbContext.DeleteOne(filter);
+
+            return new JsonResult("Deleted successfully");
+        }
+    }
+}
---
+++ LearnMongo/LearnMongo/Controllers/EmployeeController.cs
@@ -0,0 +1,101 @@
+﻿using LearnMongo.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace LearnMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public EmployeeController(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
+        {
+            this.configuration = configuration;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient mongoClient = new(configuration.GetConnectionString("EmployeeAppCon"));
+            IMongoCollection<Employee> dbContext = mongoClient.GetDatabase("testDB").GetCollection<Employee>("Employee");
+
+            List<Employee> employees = dbContext.AsQueryable().ToList();
+
+            return new JsonResult(employees);
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody] Employee employee)
+        {
+            MongoClient mongoClient = new(configuration.GetConnectionString("EmployeeAppCon"));
+            IMongoCollection<Employee> dbContext = mongoClient.GetDatabase("testDB").GetCollection<Employee>("Employee");
+
+            int lastEmployeeId = dbContext.AsQueryable().Count();
+            employee.EmployeeId = lastEmployeeId + 1;
+
+            dbContext.InsertOne(employee);
+
+            return new JsonResult("Added successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put([FromBody] Employee employee)
+        {
+            MongoClient mongoClient = new(configuration.GetConnectionString("EmployeeAppCon"));
+            IMongoCollection<Employee> dbContext = mongoClient.GetDatabase("testDB").GetCollection<Employee>("Employee");
+
+            FilterDefinition<Employee> filter = Builders<Employee>.Filter.Eq("EmployeeId", employee.EmployeeId);
+            UpdateDefinition<Employee> update = Builders<Employee>.Update.Set("EmployeeName", employee.EmployeeName)
+                .Set("Department", employee.Department)
+                .Set("DateOfJoining", employee.DateOfJoining)
+                .Set("PhotoFileName", employee.PhotoFileName);
+
+
+            dbContext.UpdateOne(filter, update);
+
+            return new JsonResult("Updated successfully");
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public JsonResult Delete([FromRoute] int id)
+        {
+            MongoClient mongoClient = new(configuration.GetConnectionString("EmployeeAppCon"));
+            IMongoCollection<Employee> dbContext = mongoClient.GetDatabase("testDB").GetCollection<Employee>("Employee");
+
+            FilterDefinition<Employee> filter = Builders<Employee>.Filter.Eq("EmployeeId", id);
+
+            dbContext.DeleteOne(filter);
+
+            return new JsonResult("Deleted successfully");
+        }
+
+        [HttpPost]
+        [Route("savefile")]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                IFormCollection httpRequest = Request.Form;
+                IFormFile postedFile = httpRequest.Files[0];
+                string fileName = postedFile.FileName;
+                string physicalPath = webHostEnvironment.ContentRootPath + "/Images/" + fileName;
+
+                using (FileStream stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                return new JsonResult(fileName);
+            }
+            catch(Exception ex) 
+            {
+                return new JsonResult(ex.Message);
+            }
+        }
+    }
+}
